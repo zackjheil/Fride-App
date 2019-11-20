eb19dc90ec3897ac61830f085d75ebd7b9d4d619
zackjheil <zh16br@brocku.ca>
2019-11-20 02:35:14 -0500

    Addition of all HTML

---
+++ ProfilePage.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+document.getElementById("Header").focus();
+}
+
+let NameChanger = function NameChanger(){
+document.getElementById("First_NameValue").innerHTML = "#FirstName_Form";
+document.getElementById("Last_NameValue").innerHTML = "#LastName_Form";
+}
---
+++ Scan.js
@@ -0,0 +1,47 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "environment" }, audio: false };
+var track = null;
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/png");
+    cameraOutput.classList.add("taken");
+    return new Promise((res, rej)=>{
+        CanvasGradient.toBlob(res,'image/jpeg');
+    });
+
+    // track.stop();
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
+function download(blob){
+    let a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download ='screenshot.jpg';
+    document.body.appendChild(a);
+    a.click();
+}
