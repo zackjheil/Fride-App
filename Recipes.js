8c7dca089373afe63199199ae31b7494ee2c68d3
zackjheil <zh16br@brocku.ca>
2019-11-20 17:42:54 -0500

    5:42 Push Update
    Variable Array Added, Function for Onclick added, Removed Window Fucker from CSS

--- Recipes.js
+++ Recipes.js
@@ -1,1 +1,13 @@
-var Ingredients = [ "chicken", "Chicken", "Rice", "rice", "Cheese", "cheese","Pasta", "pasta", "Milk", "milk", "Eggs", "eggs", "Flour", "flour","Butter", "butter", "Bacon", "bacon", "Potatoes", "potatoes", "tomatoes", "Tomatoes", "Red Onion", "red onion", "Red onion", "red Onion", "Onion", "onion", "Sugar", "sugar",   ]
+var Ingredients = ["chicken", "Chicken", "Rice", "rice", "Cheese", "cheese","Pasta", "pasta", "Milk", "milk", "Eggs", "eggs", "Flour", "flour","Butter", "butter", "Bacon", "bacon", "Potatoes", "potatoes", "tomatoes", "Tomatoes", "Red Onion", "red onion", "Red onion", "red Onion", "Onion", "onion", "Sugar", "sugar"];
+
+function WhyYouNoWork() {
+    window.location.replace("https://www.supercook.com/#/recipes");
+} 
+
+
+
+if (document.getElementById("#IngredientsList")).innerHTML = Ingredients; //[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+{
+   document.getElementById("#IngredientButton").onclick = WhyYouNoWork() 
+   window.addEventListener('load')
+};
