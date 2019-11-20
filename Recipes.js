254df825d623fa1d254ed1c83fd2a0e2916fb9b8
zackjheil <zh16br@brocku.ca>
2019-11-20 18:21:02 -0500

    6:20 Push
    Button Working in HTML, Not Working in JS

--- Recipes.js
+++ Recipes.js
@@ -5,9 +5,7 @@ function WhyYouNoWork() {
 } 
 
 
-
-if (document.getElementById("#IngredientsList")).innerHTML = Ingredients; //[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-{
-   document.getElementById("#IngredientButton").onclick = WhyYouNoWork() 
-   window.addEventListener('load')
-};
+//if (document.getElementById("IngredientsList").innerHTML = Ingredients 
+//){
+  //  onclick(WhyYouNoWork());
+//}
