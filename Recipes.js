d20ad66e36dec88a7c6d7f4fc562f5d5753a4f4d
zackjheil <zh16br@brocku.ca>
2019-11-20 16:41:06 -0500

    4:40PM Push
    CSS Content Added, JS array started

---
+++ Recipes.js
@@ -0,0 +1,1 @@
+var Ingredients = [ "chicken", "Chicken", "Rice", "rice", "Cheese", "cheese","Pasta", "pasta", "Milk", "milk", "Eggs", "eggs", "Flour", "flour","Butter", "butter", "Bacon", "bacon", "Potatoes", "potatoes", "tomatoes", "Tomatoes", "Red Onion", "red onion", "Red onion", "red Onion", "Onion", "onion", "Sugar", "sugar",   ]
