46b33bff9926d5046dfdd6f3b5b3d2293cff45cd
zackjheil <zh16br@brocku.ca>
2019-11-21 16:50:23 -0500

    4:49 Push
    Login Page and Animated Background Changed

--- ProfilePage.js
+++ ProfilePage.js
@@ -1,32 +1,3 @@
-window.onload = function() {
-document.getElementById("Header").focus();
+function WhyNoWork(){
+    window.location.href = "profileinfo.html";
 }
-
-var x = document.getElementById("FirstName_Form");
-var y = document.getElementById("LastName_Form");
-var z = document.getElementById("SubmitButton");
-
-button.onclick = function(){
-    ReplaceFirstName(x.value),
-    ReplaceLastName(y.value);
-};
-
-var ReplaceLastName = function(LastName){
-    var LastNameChange = (LastName);
-}
-
-
-
-var word = document.getElementById('word'),
-    originalStory = document.getElementById('story'),
-    button = document.getElementById("submitButton");
-
-button.onclick = function(){
-    replaceStory(word.value);
-};
-
-var replaceStory = function(userWord) {
-    var story = ("A " + userWord + " is now part of the story");
-
-    return originalStory.innerHTML = story;
-};
