d1edc7a9c5e78094cef35a3362b8c6a5b339e9de
zackjheil <zh16br@brocku.ca>
2019-11-21 01:16:30 -0500

    1:15AM Commit
    Some Changes Made to the Recipes page, Made the text Editable on the Profile Page allwoing users to change the value of their name

--- ProfilePage.js
+++ ProfilePage.js
@@ -2,7 +2,31 @@ window.onload = function() {
 document.getElementById("Header").focus();
 }
 
-let NameChanger = function NameChanger(){
-document.getElementById("First_NameValue").innerHTML = "#FirstName_Form";
-document.getElementById("Last_NameValue").innerHTML = "#LastName_Form";
+var x = document.getElementById("FirstName_Form");
+var y = document.getElementById("LastName_Form");
+var z = document.getElementById("SubmitButton");
+
+button.onclick = function(){
+    ReplaceFirstName(x.value),
+    ReplaceLastName(y.value);
+};
+
+var ReplaceLastName = function(LastName){
+    var LastNameChange = (LastName);
 }
+
+
+
+var word = document.getElementById('word'),
+    originalStory = document.getElementById('story'),
+    button = document.getElementById("submitButton");
+
+button.onclick = function(){
+    replaceStory(word.value);
+};
+
+var replaceStory = function(userWord) {
+    var story = ("A " + userWord + " is now part of the story");
+
+    return originalStory.innerHTML = story;
+};
--- Recipes.js
+++ Recipes.js
@@ -1,11 +1,10 @@
 var Ingredients = ["chicken", "Chicken", "Rice", "rice", "Cheese", "cheese","Pasta", "pasta", "Milk", "milk", "Eggs", "eggs", "Flour", "flour","Butter", "butter", "Bacon", "bacon", "Potatoes", "potatoes", "tomatoes", "Tomatoes", "Red Onion", "red onion", "Red onion", "red Onion", "Onion", "onion", "Sugar", "sugar"];
 
 function WhyYouNoWork() {
-    window.location.replace("https://www.supercook.com/#/recipes");
+    document.getElementById("#Your_Ingredients").innerHTML ="#IngredientsList";
 } 
 
 
-//if (document.getElementById("IngredientsList").innerHTML = Ingredients 
-//){
-  //  onclick(WhyYouNoWork());
-//}
+//if (document.getElementById("#IngredientsList").innerHTML = Ingredients {
+
+
