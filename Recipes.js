8920835e8d5f6aab1721223d157506a15fe3a2af
zackjheil <43187743+zackjheil@users.noreply.github.com>
2019-11-24 23:48:03 -0500

    11:47 Push
    Save Form Contents

--- Recipes.js
+++ Recipes.js
@@ -1,10 +1,9 @@
-var Ingredients = ["chicken", "Chicken", "Rice", "rice", "Cheese", "cheese","Pasta", "pasta", "Milk", "milk", "Eggs", "eggs", "Flour", "flour","Butter", "butter", "Bacon", "bacon", "Potatoes", "potatoes", "tomatoes", "Tomatoes", "Red Onion", "red onion", "Red onion", "red Onion", "Onion", "onion", "Sugar", "sugar"];
+Var Ingred =
 
-function WhyYouNoWork() {
-    document.getElementById("#Your_Ingredients").innerHTML ="#IngredientsList";
-} 
+var Form = document.querySelector("FuckShit");
+Form.addEventListener("ButtonBoi", function(event){
+    console.log("Saving Value",form.elements.value.value);
+    
+});
 
-
-//if (document.getElementById("#IngredientsList").innerHTML = Ingredients {
-
-
+if var Form (==) var Ingred
