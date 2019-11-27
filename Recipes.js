5c3adbafed3e1b0cc47d5217af37d956d33b16a1
zackjheil <43187743+zackjheil@users.noreply.github.com>
2019-11-27 12:38:06 -0500

    12:37 Push
    Fucking So Many Changes

--- Recipes.js
+++ Recipes.js
@@ -1,3 +1,65 @@
 function Chicken(){
-    window.location.href = "https://www.allrecipes.com/recipes/201/meat-and-poultry/chicken/";
+    window.location.href = "https://www.delish.com/cooking/recipe-ideas/g2972/chicken-weeknight-dinners/"
 }
+function Pasta(){
+    window.location.href = "https://www.foodnetwork.ca/everyday-cooking/photos/best-ever-pasta-recipes/#!chicken-parmesan-stuffed-shells"
+}
+
+function Eggs(){
+    window.location.href = "https://www.eggs.ca/recipes/"
+}
+function Cheese(){
+    window.location.href = "https://www.foodnetwork.ca/comfort-food/photos/top-recipes-for-cheese-lovers/#!FN001_Lynn_MacCheesePlated_Opt2_Hero"
+}
+function Flour(){
+    window.location.href = "https://www.allrecipes.com/recipes/17589/ingredients/flours/"
+}
+function Potatoe(){
+    window.location.href = "https://www.foodnetwork.ca/comfort-food/photos/tasty-potato-recipes/#!rosemary-roasted-potatoes"
+}
+function Oil(){
+    window.location.href = "https://www.allrecipes.com/recipes/17769/ingredients/cooking-oils/"
+}
+function Milk(){
+    window.location.href = "https://www.thedailymeal.com/cook/recipes-finish-gallon-milk"
+}
+
+function Butter(){
+    window.location.href = "https://www.tasteofhome.com/collection/recipes-that-are-better-with-butter/"
+}
+function Rice(){
+    window.location.href = "https://www.delish.com/cooking/recipe-ideas/g129/rice-recipes/"
+}
+function Tomatoes(){
+    window.location.href = "https://www.foodnetwork.ca/in-season/photos/fresh-tomato-recipes/#!Ina-Garten-tomato-and-goat-cheese-tart-recipe"
+}
+
+
+
+
+//<div id="DivyBoi">
+<h1 id="HeadyBoi"> Choose your Main Ingredients!</h1>
+<button id="ChickenButtonBoy" onclick="Chicken()">Chicken</button>
+<button id="PastaButtonBoy" onclick="Pasta()">Pasta</button>  
+<button id="EggsButtonBoy" onclick="Eggs()">Eggs</button>
+<button id="CheeseButtonBoy" onclick="Cheese()">Cheese</button>
+<button id="FlourButtonBoy" onclick="Flour()">Flour</button>
+<button id="PotatoeButtonBoy" onclick="Potatoe()">Potatoe</button>
+<button id="OilButtonBoy" onclick="Oil()">Oil</button>
+<button id="MilkButtonBoy" onclick="Milk()">Milk</button>
+<button id="ButterButtonBoy" onclick="Butter()">Butter</button>
+<button id="RiceButtonBoy" onclick="Rice()">Rice</button>
+<button id="TomatoesButtonBoy" onclick="Tomatoes()">Tomatoes</button>
+
+
+
+</div>
+
+
+</body>
+<script src="Recipes.js"></script> 
+
+
+
+
+</html>
